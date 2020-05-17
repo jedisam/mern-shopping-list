81c36a50e20b1aee631dc92ed58cefe481e3650b
Yididya Samuel <yidisam18@gmail.com>
2020-05-17 19:41:06 -0400

    make it better

--- routes/api/lists.js
+++ routes/api/lists.js
@@ -1,33 +1,37 @@
-const express = require ('express');
-const router = express.Router ();
+const express = require("express");
+const router = express.Router();
 // Lists model
-const List = require ('../../model/List');
+const List = require("../../model/List");
 
 // Get all lists
-router.get ('/', (req, res) => {
-  List.find ({}).sort ({date: -1}).then (lists => res.json (lists));
+router.get("/", (req, res) => {
+  List.find({})
+    .sort({ date: -1 })
+    .then((lists) => res.json(lists));
 });
 
 // add a list item, post route
 
-router.post ('/', (req, res) => {
-  let newList = new List ({
+router.post("/", (req, res) => {
+  let newList = new List({
     name: req.body.name,
   });
-  newList.save ().then (list => res.json (list)).catch (err => res.json (err));
+  newList
+    .save()
+    .then((list) => res.json(list))
+    .catch((err) => res.json(err));
 });
 
 // Delete item
 
-router.delete ('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   let id = req.params.id;
-  let query = {_id: id};
-  List.findOne (query, (err, list) => {
+  let query = { _id: id };
+  List.findOne(query, (err, list) => {
     if (err) {
-      console.log ('Heyyyyyyyyy');
-      res.status (404).json ('list not found');
+      res.status(404).json("list not found");
     } else {
-      List.deleteOne (query).then (res.json (list));
+      List.deleteOne(query).then(res.json(list));
     }
   });
 });
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 require("dotenv/config");
-const Lists = require('./routes/api/lists')
+const Lists = require("./routes/api/lists");
+const path = require('path')
 
 const app = express();
 
@@ -11,16 +12,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const uri = process.env.db;
- mongoose.connect(uri,{ useNewUrlParser: true, useUnifiedTopology: true  })
- const connection = mongoose.connection
-connection.once('open',()=>{
-    console.log(`Succsessfully connected to DB`)
-})
-app.get('/',(req,res)=>{
-  res.send('Haloo')
-})
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log(`Succsessfully connected to DB`);
+});
+app.get("/", (req, res) => {
+  res.send("Haloo");
+});
 
-app.use('/api/lists',Lists);
+app.use("/api/lists", Lists);
+
+// serve static asset if in production
+if(process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('Client/build'))
+
+  app.get('*',(req,res) =>{
+    res.sendFile(path.resolve(__dirname,'Client','build','index.html'))
+  } )
+}
 
 const PORT = process.env.PORT || 8000;
 
